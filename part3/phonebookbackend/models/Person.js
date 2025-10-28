54a5642ab2c93ea40703cb9be95313b9de1594f4
thezmmm <1563081082@qq.com>
2025-10-28 14:52:14 +0200

    feat: add validation

--- part3/phonebookbackend/models/Person.js
+++ part3/phonebookbackend/models/Person.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose")
 
+const numberValidator = function(v) {
+    if (!v) return false
+
+    const regex = /^\d{2,3}-\d{5,}$/
+    return regex.test(v)
+}
+
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: numberValidator,
+            message: props => `${props.value} is not a valid phone number! Format must be XX-XXXXXXX or XXX-XXXXXXXX`
+        }
+    },
 })
 
 personSchema.set('toJSON', {
