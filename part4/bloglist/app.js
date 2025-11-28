0920b9dbd066720ab1cbaf69548e543d257573fe
thezmmm <1563081082@qq.com>
2025-11-28 17:22:38 +0200

    feat: add testing api in backend

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -26,6 +26,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor,blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
 
 app.use(errorHandler)
 
